b49d1516d503acea4c25e28b6d8aa3b9498a25a2
jeremy miller <jeremyemiller@gmail.com>
2018-11-30 21:05:12 -0800

    add callback to error on no host match

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ class Enveigle extends Command {
       description: 'local environment name',
       default: 'development',
     }),
+    cbpath: flags.string({
+      char: 'c',
+      description: 'callback path',
+      default: 'lib/trellis/plugins/callback',
+    }),
   }
 
   async run() {
@@ -23,30 +28,40 @@ class Enveigle extends Command {
 
     const tasks = new Listr([
       {
-        title: 'Copy enveigle.yml',
+        title: 'Copy enveigle.yml & callback',
         task: () => {
-          const source = path.join(__dirname, '../templates/enveigle.yml')
-          fs.copyFileSync(source, 'enveigle.yml')
+          const enveigle = path.join(__dirname, '../templates/enveigle.yml')
+          const callback = path.join(__dirname, '../callback_plugins/no_hosts_matched.py')
+          fs.copyFileSync(callback, (flags.cbpath + '/no_hosts_matcheded.py'))
+          fs.copyFileSync(enveigle, 'enveigle.yml')
         }
       },
       {
         title: 'Template .env files to local system',
-        task: (_, task) => {
-          task.output = `$ ansible-playbook enveigle.yml -e env=${flags.env}`
-          return execa('ansible-playbook', ['enveigle.yml', `-e env=${flags.env}`])
-        }
+        task: (_, task) =>  execa('ansible-playbook', ['enveigle.yml', `-e env=${flags.env}`])
+        .catch((result) => {
+          if (result.code = 10) {
+            throw new Error('Could not match supplied host pattern')
+          } else {
+            task.skip('Something went wrong. Try again!')
+          }
+        })
       },
       {
         title: 'Remove enveigle.yml',
-        task: () => fs.unlinkSync('enveigle.yml')
+        task: () => {
+          fs.unlinkSync('enveigle.yml')
+          fs.unlinkSync(flags.cbpath + '/no_hosts_matcheded.py')
+        }
       },
     ])
 
     tasks.run().catch(err => {
-      console.error(err)
       console.error('##########################################')
       console.error('Abort! Something went wrong')
       console.error('You have to delete enveigle.yml manually')
+      console.error('Error message:')
+      console.error(err)
     })
   }
 }
